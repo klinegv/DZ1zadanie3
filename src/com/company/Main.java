c204a4a04c5bf21e84159e282480d9ee7c9f3507
Alex Genov <alexgenov@MacBook-Pro-Alex.local>
2018-01-24 02:25:20 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        int p = 84;
+        int a = p / 4;
+        System.out.println(a);
+    }
+}
+ 
+
+
